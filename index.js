acdc72daed72e476f43b057ddab0dec9571c9407
Jgcbitj <joao.boemer@edu.univali.br>
2022-06-30 12:49:48 -0300

    Adicionando bodyParser

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const Posts = require('./app/models/Posts')
+const bodyParser = require('body-parser')
 const PORT = 8080
 
 
-//require("./app/routes/index")(app);
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 /// Rotas
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+
+
 
 app.listen(
     PORT,
